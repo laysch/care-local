49dab91a6580bd7b4a96e9830e4d53a349de0d56
rjkozlowskijr <rjkozlowskijr@gmail.com>
2025-03-03 11:02:15 -0500

    max file size in upload

--- inc/uploadAvatar.php
+++ inc/uploadAvatar.php
@@ -11,22 +11,26 @@ if (isset($_POST['upload'])) {
     $file = "../img/avatar/" . basename($_FILES["avatar"]["name"]);
     $allowedFiles = ['jpg', 'jpeg', 'png', 'gif'];
     $fileType = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+    $maxFileSize = 5 * 1024 * 1024; // 5mb
 
     try {
         if (!in_array($fileType, $allowedFiles)) {
-            throw new Exception("Please use one of the following file types: " . print_r($allowedFiles));
+            throw new Exception("Please use one of the following file types: " . implode(", ", $allowedFiles));
+        } 
+
+        if ($_FILES["avatar"]["size"] > $maxFileSize) {
+            throw new Exception("Please use an image unde 5MB.")
+        
+        if (move_uploaded_file($_FILES["avatar"]["tmp_name"], $file)) {
+            $query = "UPDATE users SET avatar = ? WHERE id = ?";
+            $stmt = $conn->prepare($query);
+            $stmt->bind_param("si", basename($file), $userId);
+            $stmt->execute();
+            header("Location: /profile.php");
         } else {
-            if (move_uploaded_file($_FILES["avatar"]["tmp_name"], $file)) {
-                $query = "UPDATE users SET avatar = ? WHERE id = ?";
-                $stmt = $conn->prepare($query);
-                $stmt->bind_param("si", basename($file), $userId);
-                $stmt->execute();
-                header("Location: /profile.php");
-            } else {
-                throw new Exception("Avatar upload failed");
-            }
+            throw new Exception("Avatar upload failed");
         }
-    } catch (Exception $e) {
+} catch (Exception $e) {
         echo $e->getMessage();
     }
 }
