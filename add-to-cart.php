cdd835625a88a63cbd43f20666c8dfe1970dd6de
laila <100658865+laysch@users.noreply.github.com>
2025-03-04 15:53:28 -0500

    Create add-to-cart.php

---
+++ add-to-cart.php
@@ -0,0 +1,29 @@
+<?php
+// Start the session
+session_start();
+
+// Check if the form is submitted
+if (isset($_POST['add_to_cart'])) {
+    // Get job details from the form
+    $jobId = $_POST['job_id'];
+    $jobTitle = $_POST['job_title'];
+    $jobDescription = $_POST['job_description'];
+    $jobSkills = $_POST['job_skills'];
+
+    // Initialize the cart if it doesn't exist
+    if (!isset($_SESSION['job_cart'])) {
+        $_SESSION['job_cart'] = [];
+    }
+
+    // Add the job to the cart
+    $_SESSION['job_cart'][$jobId] = [
+        'title' => $jobTitle,
+        'description' => $jobDescription,
+        'skills' => $jobSkills
+    ];
+
+    // Redirect to the job cart page
+    header('Location: job-cart.php');
+    exit;
+}
+?>
