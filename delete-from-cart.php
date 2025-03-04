836f220b3992694597e991cb7af70f760b78de10
laila <100658865+laysch@users.noreply.github.com>
2025-03-04 16:26:41 -0500

    Create delete-from-cart.php

---
+++ delete-from-cart.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+// Check if the job ID is provided
+if (!isset($_POST['job_id'])) {
+    header('Location: job-cart.php');
+    exit;
+}
+
+$jobId = $_POST['job_id'];
+
+// Check if the job exists in the cart
+if (isset($_SESSION['job_cart'][$jobId])) {
+    // Remove the job from the cart
+    unset($_SESSION['job_cart'][$jobId]);
+}
+
+// Redirect back to the job cart page
+header('Location: job-cart.php');
+exit;
+?>
