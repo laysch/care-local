73eac8150ce33800694114d7428de8a2c8ae1b1c
laila <100658865+laysch@users.noreply.github.com>
2025-03-04 04:29:40 -0500

    Update profile.php

--- profile.php
+++ profile.php
@@ -80,17 +80,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $user['bio'] = $_POST['bio'];
     $user['skills'] = isset($_POST['skills']) ? $_POST['skills'] : [];
 
- <!-- Check if avatar exists -->
-    <?php if (isset($row['avatar']) && !empty($row['avatar'])): ?>
-        <img src="<?php echo "img/avatar/" . htmlspecialchars($row['avatar']); ?>" alt="User Avatar">
-    <?php else: ?>
-        <img src="img/default-avatar.png" alt="Default User Avatar">
-    <?php endif; ?>
+<!-- Check if avatar exists -->
+<?php if (isset($row['avatar']) && !empty($row['avatar'])): ?>
+    <img src="<?php echo "img/avatar/" . htmlspecialchars($row['avatar']); ?>" alt="User Avatar">
+<?php else: ?>
+    <img src="img/default-avatar.png" alt="Default User Avatar">
+<?php endif; ?>
 
-    <form action="inc/uploadAvatar.php" method="POST" enctype="multipart/form-data">
-        <input type="file" name="avatar" accept="image/*">
-        <button type="submit" name="upload">Upload</button>
-    </form>
+<form action="inc/uploadAvatar.php" method="POST" enctype="multipart/form-data">
+    <input type="file" name="avatar" accept="image/*">
+    <button type="submit" name="upload">Upload</button>
+</form>
+
         }
     }
 }
@@ -341,19 +342,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         <?php endforeach; ?>
                     </div>
 
-                    <!-- Check if avatar exists -->
-    <?php if (isset($row['avatar']) && !empty($row['avatar'])): ?>
-        <img src="<?php echo "img/avatar/" . htmlspecialchars($row['avatar']); ?>" alt="User Avatar">
-    <?php else: ?>
-        <img src="img/default-avatar.png" alt="Default User Avatar">
-    <?php endif; ?>
+                <!-- Check if avatar exists -->
+<?php if (isset($row['avatar']) && !empty($row['avatar'])): ?>
+    <img src="<?php echo "img/avatar/" . htmlspecialchars($row['avatar']); ?>" alt="User Avatar">
+<?php else: ?>
+    <img src="img/default-avatar.png" alt="Default User Avatar">
+<?php endif; ?>
 
-    <form action="inc/uploadAvatar.php" method="POST" enctype="multipart/form-data">
-        <input type="file" name="avatar" accept="image/*">
-        <button type="submit" name="upload">Upload</button>
-    </form>
-                    
-                </form>
+<form action="inc/uploadAvatar.php" method="POST" enctype="multipart/form-data">
+    <input type="file" name="avatar" accept="image/*">
+    <button type="submit" name="upload">Upload</button>
+</form>
+
             </div>
         </div>
     </div>
