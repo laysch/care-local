54808104993a547366735e6c2d6080d3218a619a
shaker-omar <shako@farmingdale.edu>
2025-04-04 15:56:15 -0400

    attempt to fix

--- search-users.php
+++ search-users.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 include 'sidebar.php';
 ?>
 
-<?php include 'sidebar.php'; ?>
+<?php //include 'sidebar.php'; ?>
 <?php   
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
