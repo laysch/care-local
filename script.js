7a1d7bfe34396488455552247015174394ccba73
shaker-omar <shako@farmingdale.edu>
2025-04-06 13:18:09 -0400

    skill dropdown menu added to javascript file

--- script.js
+++ script.js
@@ -81,3 +81,9 @@ function setPage(pageName) {
     toggleMenu(); // Close menu
 }
 
+// Function to toggle the skills dropdown visibility
+function toggleSkills() {
+    const dropdownMenu = document.getElementById('dropdown-skills');
+    // Toggle the 'show' class to display or hide the dropdown
+    dropdownMenu.classList.toggle('show');
+}
